e9d79897a26ea76677d0004dd058807b581a176b
Cookie <122497552+CookieDevCoder@users.noreply.github.com>
2025-09-22 22:15:29 -0700

    Added ReadMe and fixed popup

---
+++ README.md
@@ -0,0 +1,2 @@
+## Test Extension ##
+Planning to create Chrome Extensions soon, here is a simple script to make an alert pop up
--- popup.js
+++ popup.js
@@ -1,6 +1,14 @@
 document.getElementById("clickMe").addEventListener("click", () => {
-  chrome.scripting.executeScript({
-    target: {tabId: chrome.tabs.TAB_ID_CURRENT},
-    func: () => alert("Hello from your extension!")
+  // Find the active tab
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    let tab = tabs[0];
+    if (tab && tab.id) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+          alert("Hello from your extension!");
+        }
+      });
+    }
   });
 });
